ca08bc4db75fb4945f3d6688125a94f702987931
Alvaro Radigales <30388228+doolanshire@users.noreply.github.com>
2021-03-01 16:49:55 +0100

    Added life coefficients CSV file
    Helper function to follow

---
+++ maneuver_rules_1922/helper_functions.py
@@ -0,0 +1,14 @@
+"""
+###########################################
+## NAVAL WAR COLLEGE MANEUVER RULES 1922 ##
+###########################################
+A collection of helper functions and tools
+to make the preparation and conduct of the
+game easier
+"""
+
+# Dictionary values
+
+
+def life_value(ship_class, displacement):
+    return 0
