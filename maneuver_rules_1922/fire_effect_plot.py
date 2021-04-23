445ed9a95898471ab01338f897b8b7d635e3a317
Alvaro Radigales <30388228+doolanshire@users.noreply.github.com>
2021-04-23 14:02:06 +0200

    6-in/50 fire effect table partially implemented
    Also rates of fire for all guns at all ranges

---
+++ maneuver_rules_1922/fire_effect_plot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+"""This short script makes a plot of all columns of a fire effect table (with range as the indes) to visually
+check for bad input values.
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+file_name = '6-in-50_percent_top.csv'
+file_directory = 'fire_effect_tables/1922/6-in-50/'
+file_path = file_directory + file_name
+
+gun_data = pd.read_csv(file_path, index_col='range', na_values="--", dtype=float)
+
+print(gun_data)
+gun_data.plot(title=file_name, marker='o', ms=3)
+plt.show()
