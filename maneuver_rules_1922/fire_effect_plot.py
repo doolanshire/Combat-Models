948ddf0fa44a5d40fee8d303208da6807c36c1a2
Alvaro Radigales <30388228+doolanshire@users.noreply.github.com>
2021-04-24 22:41:35 +0200

    Some improvements to the fire effect plot helper file

--- maneuver_rules_1922/fire_effect_plot.py
+++ maneuver_rules_1922/fire_effect_plot.py
@@ -5,12 +5,15 @@ check for bad input values.
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file_name = '6-in-53_percent_plane.csv'
-file_directory = 'fire_effect_tables/1922/6-in-53/'
-file_path = file_directory + file_name
+fire_effect_tables_edition = "1922"
+gun_designation = '6-in-50'
+spot_type = 'top'
+
+file_path = "fire_effect_tables/{}/{}/{}_percent_{}.csv".format(fire_effect_tables_edition, gun_designation,
+                                                                gun_designation, spot_type)
 
 gun_data = pd.read_csv(file_path, index_col='range', na_values="--", dtype=float)
 
 print(gun_data)
-gun_data.plot(title=file_name, marker='o', ms=3)
+gun_data.plot(title="{} {}".format(gun_designation, spot_type), marker='o', ms=3)
 plt.show()
